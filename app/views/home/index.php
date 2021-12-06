2e6def015c0e0796ce585f3938b820912a838ed1
Mark-Inholland <mark.dehaan@inholland.nl>
2021-12-06 21:18:46 +0100

    Fixed typo in heading

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,7 +4,7 @@ include __DIR__ . '/../header.php';
 <section class="py-5 text-center container">
 <div class="row py-lg-5">
   <div class="col-lg-6 col-md-8 mx-auto">
-    <h1 class="fw-light">Image Gallery Assignemnt</h1>
+    <h1 class="fw-light">Image Gallery Assignment</h1>
     <p class="lead text-muted">This image gallery website is incomplete, can you finish it?</p>
     <p>
       <a href="#" class="btn btn-primary my-2">View gallery</a>
