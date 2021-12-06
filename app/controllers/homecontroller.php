6dbb23cf6a5e9e585df7c64d9925bebff73d77d1
Mark-Inholland <mark.dehaan@inholland.nl>
2021-12-06 21:16:25 +0100

    Removed unnecessary stuff

--- app/controllers/homecontroller.php
+++ app/controllers/homecontroller.php
@@ -2,14 +2,8 @@
 
 class HomeController
 {
-
     public function index()
     {
         require __DIR__ . '/../views/home/index.php';
-    }
-
-    public function about()
-    {
-        require __DIR__ . '/../views/home/about.php';
     }
 }
--- app/dbconfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-$servername = "mysql";
-$username = "root";
-$password = "secret123";
-$database = "developmentdb";
-?>
